825c2814205f7ae57a9be81f78f2242d98c92c1c
AM-5006 <aniketmandrekar72576@gmail.com>
2024-01-14 02:24:00 +0530

    Updated tasks

--- app/tasks.py
+++ app/tasks.py
@@ -6,12 +6,12 @@ from celery.utils.log import get_task_logger
 # from channels.layers import get_channel_layer
 
 from django.utils import timezone
-from django.contrib.auth.models import User
-
-from .models import Message
 
 @shared_task
 def save_user_messages(user_id, messages):
+    from django.contrib.auth.models import User
+    from .models import Message
+    
     user = User.objects.get(pk=user_id)
     
     for message_text in messages:
